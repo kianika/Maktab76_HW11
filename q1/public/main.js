473fbf5ae34cf4c15c4e9327eac6c314607392f9
kianika <mona_yaghoubi@gmail.com>
2022-06-08 13:42:33 +0200

    v1

---
+++ q1/public/main.js
@@ -0,0 +1,16 @@
+var tr = document.querySelectorAll('tr');
+console.log(tr);
+tr[2].style.backgroundColor = "#bfdbfe";
+tr[3].style.backgroundColor = "#f5d0fe";
+tr[4].style.backgroundColor = "#d1fae5";
+tr[5].style.backgroundColor = "#fecdd3";
+tr[6].style.backgroundColor = "#fef9c3";
+tr[7].style.backgroundColor = "#e0e7ff";
+tr[8].style.backgroundColor = "#f9fafb";
+tr[9].style.backgroundColor = "#1f2937";
+tr[9].style.color = "white";
+
+
+
+
+
