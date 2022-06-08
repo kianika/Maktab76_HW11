295b7d3847d1e8b7afae6395bcc11f36629bbcf4
kianika <mona_yaghoubi@gmail.com>
2022-06-08 17:02:12 +0200

    v4

--- q1/public/main.js
+++ q1/public/main.js
@@ -12,5 +12,41 @@ tr[9].style.color = "white";
 
 
 
+//q2 answer
+var btn = document.createElement("button");
+btn.className = "bg-blue-500 rounded text-white py-2 px-4 m-4 hover:bg-green-600";
+btn.textContent = "Remove row";
+
+document.getElementById('c1').prepend(btn);
+btn.onclick = function removeRow(){
+    for(i=1; i<=5; i++){
+        tr[i].remove();
+    }
+}
+
+
+//q3 answer
+var addbtn = document.createElement("button");
+addbtn.className = "bg-blue-500 rounded text-white py-2 px-4 m-4 hover:bg-green-600";
+addbtn.textContent = "Add row";
+document.getElementById('c1').append(addbtn);
+
+
+ addbtn.onclick = function addRow(){
+   for(i=0; i<=2; i++){
+    let tr = document.createElement("tr");
+    for(j=0; j<3; j++){
+        let td = document.createElement("td");
+        td.className = "px-6 pt-3 pb-2 text-medium col-3 text-left font-semibold";
+        //td.textContent = "cell";
+        tr.prepend(td);
+    }
+    tr.style.backgroundColor = "orange";
+    document.getElementById('tb1').prepend(tr);
+}
+} 
+
+ 
+
 
 
